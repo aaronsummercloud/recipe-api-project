fc81fdfc020cf8927e9a1061500eca80e2a39ea9
Aaron Summer <aaron@summer-cloud.ca>
2024-07-29 11:40:19 -0700

    Flake8'ed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -23,10 +23,9 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """ test waiting for database when getting OperationalError """
-        # Simulate 5 errors then a TRUE while waiting for database engine to be ready
+        # Simulate 5 errors then a TRUE while waiting for database
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
